220e4a85164026c0b1f6e2d02445463e1081d979
Christopher Statham <spyman40@gmail.com>
2024-04-13 21:07:16 -0400

    stuff and things

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,20 @@
+provider "google" {
+  project     = "mythrilgameservers"
+  region      = "us-central1"
+}
+
 terraform {
   backend "gcs" {
       bucket = "cloudgamingstate"
       prefix = "workspaces"
   }
+}
+
+resource "google_compute_disk" "primary" {
+  name  = "game-storage"
+  type  = "pd-standard"
+  zone  = "us-central1-c"
+  size  = 30
+
+  physical_block_size_bytes = 4096
 }
